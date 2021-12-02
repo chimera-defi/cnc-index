dd6141219eea35a10ff54f52a150f3457d66f596
Chimera Defi <chimera_defi@protonmail.com>
2021-12-02 21:43:02 +0530

    working yield redirect

--- contracts/cnc-yield/contracts/YieldRedirector.sol
+++ contracts/cnc-yield/contracts/YieldRedirector.sol
@@ -7,47 +7,47 @@ import "./interfaces/IUniLikeSwapRouter.sol";
 
 interface IStrategy {
     function harvest() external;
+    function want() external returns (address);
 }
 
-interface IPotPool {
-    function notifyRewardAmount(uint256 reward) external;
+// https://github.com/curvefi/multi-rewards/blob/master/contracts/MultiRewards.sol
+interface IMutliRewards {
+    function notifyRewardAmount(address _rewardsToken, uint256 reward) external;
 }
 
 contract YieldRedirector is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    // We recv excesss want tokens from a strategy
+    // We recv excesss want tokens from a strategy by calling harvest on it
     // We then need to sell the tokens to our secondary want token, e.g. kllima
     // Finally we send the new tokens and call notifyRewardAmount on a SNX/harvest potpool style rewarder
     // Users can then claim their rewards from it.
     address public keeper;
-    IERC20 public want2;
+    IERC20 public target;
     IERC20 public want;
     IStrategy public underlyingStrategy;
-    IPotPool public rewarderMasterchef;
+    address public rewarder;
 
-    IUniLikeSwapRouter public constant SUSHI_ROUTER =
-        IUniLikeSwapRouter(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
     IUniLikeSwapRouter public ROUTER;
 
     constructor(
-        address _vault,
-        IERC20 _want2,
-        IERC20 _want,
+        IERC20 _target,
         IStrategy _strat,
-        IPotPool _potpool
+        address _router,
+        address _rewarder // no check as this needs to be set later
     ) public {
-        want2 = _want2;
-        want = _want;
-        // Get Decimals
-        // DECIMALS = ERC20(address(want)).decimals();
-        ROUTER = SUSHI_ROUTER;
-        want.safeApprove(address(ROUTER), type(uint256).max);
-        want2.safeApprove(address(ROUTER), type(uint256).max);
+        target = _target;
+        keeper = msg.sender;
+        ROUTER = IUniLikeSwapRouter(_router);
 
         underlyingStrategy = _strat;
-        rewarderMasterchef = _potpool;
+        want = IERC20(underlyingStrategy.want());
+        want.safeApprove(address(ROUTER), type(uint256).max);
+        target.safeApprove(address(ROUTER), type(uint256).max);
+
+        rewarder = _rewarder;
+        target.safeApprove(_rewarder, type(uint256).max);
     }
 
     modifier onlyAuthorized() {
@@ -60,26 +60,34 @@ contract YieldRedirector is Ownable {
         keeper = _keeper;
     }
 
+    function setRewarder(address _rewarder) external onlyAuthorized {
+        rewarder = _rewarder;
+        target.safeApprove(rewarder, type(uint256).max);
+    }
+
+    function setStrategy(IStrategy _strategy) external onlyAuthorized {
+        underlyingStrategy = _strategy;
+    }
+
     function harvest() public onlyAuthorized {
         // harvest to get tokens then swap tokens
         underlyingStrategy.harvest();
         uint256 bal = want.balanceOf(address(this));
         if (bal > 0) {
-            _fromWant1ToWant2(bal, 0);
+            _fromWantToTarget(bal, 0);
         }
         updateRewards();
     }
 
     function updateRewards() public onlyAuthorized {
         // send to rewarder and notify to update distribution rate
-        uint256 bal2 = want2.balanceOf(address(this));
-        if (bal2 > 0) {
-            want2.safeTransfer(address(rewarderMasterchef), bal2);
-            rewarderMasterchef.notifyRewardAmount(bal2);
+        uint256 bal = target.balanceOf(address(this));
+        if (bal > 0 && rewarder != address(0)) {
+            IMutliRewards(rewarder).notifyRewardAmount(address(target), bal);
         }
     }
 
-    // Swap from AAVE to Want
+    // Swap from Want to target
     ///@param amountToSwap Amount of AAVE to Swap, NOTE: You have to calculate the amount!!
     ///@param multiplierInWei pricePerToken including slippage, will be divided by 10 ** 18
     function manualSwap(uint256 amountToSwap, uint256 multiplierInWei)
@@ -90,20 +98,24 @@ contract YieldRedirector is Ownable {
             10**18
         );
 
-        _fromWant1ToWant2(amountToSwap, amountOutMinimum);
+        _fromWantToTarget(amountToSwap, amountOutMinimum);
     }
 
-    function _fromWant1ToWant2(uint256 amountIn, uint256 minOut) internal {
+    function recover(address token) external onlyAuthorized {
+        IERC20(token).safeTransfer(msg.sender, IERC20(token).balanceOf(address(this)));
+    }
+
+    function _fromWantToTarget(uint256 amountIn, uint256 minOut) internal {
         address[] memory path = new address[](2);
         path[0] = address(want);
-        path[1] = address(want2);
-        if (want2 != want) {
+        path[1] = address(target);
+        if (target != want && amountIn > 0) {
             ROUTER.swapExactTokensForTokens(
                 amountIn,
                 minOut,
                 path,
                 address(this),
-                now
+                now + 100
             );
         }
     }
